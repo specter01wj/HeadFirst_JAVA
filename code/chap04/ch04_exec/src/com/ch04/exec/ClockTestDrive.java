08daf9e8f0aac15986301b745059b73e9d06c768
specter01wj <specter01wj@gmail.com>
2019-03-06 01:44:18 -0500

    02/03/2019 v1

--- code/chap04/ch04_exec/src/com/ch04/exec/ClockTestDrive.java
+++ code/chap04/ch04_exec/src/com/ch04/exec/ClockTestDrive.java
@@ -3,8 +3,26 @@ package com.ch04.exec;
 public class ClockTestDrive {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Clock c = new Clock();
+		
+		c.setTime("12:34PM");
+		String tod = c.getTime();
+		
+		System.out.println("time: " + tod);
 	}
 
+}
+
+class Clock {
+	String time;
+
+	String getTime() {
+		return time;
+	}
+
+	void setTime(String time) {
+		this.time = time;
+	}
+	
+	
 }
