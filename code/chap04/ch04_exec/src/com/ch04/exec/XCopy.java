6f1cc58debbc592e4b8e961c5d19683a75525f3b
specter01wj <specter01wj@gmail.com>
2019-03-06 01:24:51 -0500

    02/03/2019 v1

--- code/chap04/ch04_exec/src/com/ch04/exec/XCopy.java
+++ code/chap04/ch04_exec/src/com/ch04/exec/XCopy.java
@@ -3,8 +3,16 @@ package com.ch04.exec;
 public class XCopy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int orig = 42;
+		
+		XCopy x = new XCopy();
+		int y = x.go(orig);
+		System.out.println(orig + "->" + y);
+	}
+	
+	int go(int arg) {
+		arg *= 2;
+		return arg;
 	}
 
 }
