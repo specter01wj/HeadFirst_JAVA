23c6c4210e7e549649cdeeb5f055013b266e218b
specter01wj <specter01wj@gmail.com>
2019-03-21 01:27:53 -0400

    03/21/2019 v1

--- code/chap05/ch04_exec/src/com/ch05/exec/SimpleDotComTestDrive.java
+++ code/chap05/ch04_exec/src/com/ch05/exec/SimpleDotComTestDrive.java
@@ -3,8 +3,13 @@ package com.ch05.exec;
 public class SimpleDotComTestDrive {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		SimpleDotCom dot = new SimpleDotCom();
+		int[] locations = {2, 3, 4};
+		dot.setLocationCells(locations);
+		String userGuess = "10";
+		String result = dot.checkYourself(userGuess);
+		System.out.println("You -> " + result);
 	}
 
 }
